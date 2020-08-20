c3033606662a948fc0fb137dbdbbbbcf5b2803a3
Guillaume1979 <guibetguillaume@gmail.com>
2020-08-20 16:28:43 +0200

    adding panel component

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material/material.module';
 import {RouterModule} from '@angular/router';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { WelcomeComponent } from './content/welcome/welcome.component';
+import {HeaderComponent} from './shared/header/header.component';
+import {FooterComponent} from './shared/footer/footer.component';
+import {SidebarComponent} from './shared/sidebar/sidebar.component';
+import {WelcomeComponent} from './content/welcome/welcome.component';
+import {PanelComponent} from './content/panel/panel.component';
 
 @NgModule({
   declarations: [
@@ -16,17 +17,20 @@ import { WelcomeComponent } from './content/welcome/welcome.component';
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     RouterModule.forRoot([
-        {path: '' , component: WelcomeComponent}
-      ])
+      {path: '', component: WelcomeComponent},
+      {path: 'panel', component: PanelComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
